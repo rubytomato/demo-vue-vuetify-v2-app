8c4ea497047ec9f8aa929b2c4d81224444850698
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2020-03-04 20:59:13 +0900

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  plugins: ['vuetify'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'vuetify/no-deprecated-classes': 'error'
   }
-};
+}
